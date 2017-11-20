6093da1d3ff18d61c4f26545e22c4e91cefff699
Justin Jungst <jjungstprogramming@gmail.com>
2017-11-20 10:04:40 -0600

    edit expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense, 
   setExpenses, 
   startSetExpenses,
-  startRemoveExpense } from '../../actions/expenses';
+  startRemoveExpense,
+  startEditExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase.js';
 
@@ -53,6 +54,28 @@ test('should setup editExpense action object', () => {
     updates: {
       note: 'New note value'
     }
+  });
+});
+
+test('should edit expense in firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[1].id;
+  const updates = {
+    description: 'Water Bill',
+    note: 'Editing',
+    amount: 10000
+  }
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
   });
 });
 
